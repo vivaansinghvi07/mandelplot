4510ab3c5c1e8d36f4a36e86390b1effca1ca102
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-30 23:09:25 -0400

    created ploit function

---
+++ plot.js
@@ -0,0 +1,127 @@
+class MandelPlot {
+
+    // creates a new plot element with the
+    constructor(screen, density, depth, bounds, workers) {
+        // sets the bounds for the plot
+        this.bounds = bounds;  
+
+        // stores screen width and height
+        this.screen =  screen;
+
+        // sets the width of each point in pixels
+        this.pointWidth = screen.width / density;
+
+        // sets the depth 
+        this.depth = depth;
+
+        // sets the number of workers generating layout
+        this.workers = workers;
+        this.colors = new Array(workers);
+        this.colors2D = null;   // stores colors in a pure 2d array, not a list of arrays of arrays
+
+        // counts to make sure the thing is ready to display
+        this.displayCount = 0;
+
+        // determines height density
+        this.counts = {
+            x: density,
+            y: parseInt(Math.floor(screen.height / this.pointWidth))
+        }
+
+        // stores the indexes at which the workers will work
+        this.workerIndeces = new Array();
+        for (let i = 0; i < workers; i++) {
+            this.workerIndeces[i] = Math.floor(this.counts.y / this.workers * (i + 1));
+        }
+
+        // creates new array of rows and colors
+        this.rows = new Array(this.counts.y);   
+        for (let i = 0; i < this.counts.y; i++) {
+            this.rows[i] = new Array(this.counts.x);
+        }
+
+        // generates the points to analyze
+        this.generateComplexPoints();
+
+    }
+
+    // generate an array of points
+    generateComplexPoints() {
+        
+        // go through the array and calculate a point for each thing
+        this.rows.forEach((row, y) => {
+            for (let x = 0; x < this.counts.x; x++) {
+                row[x] = this.calcPoint(y, x);
+            }
+        });
+
+    }
+
+    // calculates the point at the location given
+    calcPoint(y, x) {
+        return {
+            x: this.bounds.lowerX + (this.bounds.upperX - this.bounds.lowerX) / this.counts.x * x,
+            y: this.bounds.lowerY + (this.bounds.upperY - this.bounds.lowerY) / this.counts.y * y
+        }
+    }
+
+    // uses multithreading to determine the colors at each location
+    display(ctx) {
+
+        // finds colors
+        for (let i = 0; i < this.workers; i++) {
+
+            // creates worker and rows on which it will perform work
+            let worker = new Worker('worker.js');
+            let material = new Array();
+
+            // adds the arrays to the materials
+            for (let rowCount = (i === 0) ? 0 : this.workerIndeces[i - 1]; rowCount < this.workerIndeces[i]; rowCount++) {
+                material.push(this.rows[rowCount]);
+            }
+
+            // sends and receievs message from the worker
+            worker.addEventListener('message', (event) => {
+                this.colors[event.data[1]] = event.data[0];
+                this.displayCount++;
+
+                // checks if displayable
+                if (this.displayCount == this.workers) {
+                    this.displayGraph(ctx);
+                }
+            });
+            worker.postMessage([material, this.depth, i]);
+        }
+        
+    }
+
+    displayGraph(ctx) {
+        
+        // stores colors into 2d array
+        this.colors2D = new Array();
+        for (let i = 0; i < this.workers; i++) {
+            this.colors[i].forEach((row) => {
+                this.colors2D.push(row);
+            })
+        }
+
+        console.log(this.colors2D);
+        console.log(this.rows);
+
+        this.colors2D.forEach((row, y) => {
+            row.forEach((color, x) => {
+
+                // calcualtes spot for rectangle
+                let newX = (x / this.counts.x) * this.screen.width;
+                let newY = (y / this.counts.y) * this.screen.height;
+
+                ctx.fillStyle = color;
+                ctx.fillRect(newX, newY, this.pointWidth*1.5, this.pointWidth*1.5);
+
+            })
+        })
+
+    }
+
+
+}
